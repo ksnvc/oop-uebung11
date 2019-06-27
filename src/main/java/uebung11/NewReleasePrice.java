ca263b040925f3df83549a2f6f40ede4545c2267
Sara Kosanovic <sarakosanovic@saras-mbp.speedport_w_724v_typ_a_05011603_06_001>
2019-06-27 22:12:53 +0200

    loesung

--- src/main/java/uebung11/NewReleasePrice.java
+++ src/main/java/uebung11/NewReleasePrice.java
@@ -5,9 +5,11 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
    public double getCharge(int daysRented){
         return daysRented * 3;
     }
+
     public double getFrequentRenterPoints(int daysRented){
         if( daysRented > 1)
             return 2;
